726a06bb13a061dcb53884ced4d5aeb9cbe4550e
paulo56ser <paulo.fh@icloud.com>
2022-11-28 18:27:30 +0000

    att 11/28

---
+++ 02-builtin-functions/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  instance_number = lookup(var.instance_number, var.env)
+
+  file_ext    = "zip"
+  object_name = "meu-arquivo-gerado-de-um-template"
+
+  common_tags = {
+    "Owner" = "Paulo Sérgio"
+    "Year"  = "2022"
+  }
+}
